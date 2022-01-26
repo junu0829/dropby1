8cc87cde959362a671ba38d26ef1116af09cdab3
junu0829 <89297716+junu0829@users.noreply.github.com>
2022-01-26 10:29:28 +0900

    Create fetch.js

---
+++ frontend/src/services/fetch.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const axiosInstance = axios.create();
+// fetch data from API using config
+// config contains url, method, headers, params, data, ...
+// reference: https://github.com/axios/axios#request-config
+const fetchApi = async (config) => {
+    try {
+        const { data } = await axiosInstance(config);
+        return [data, null];
+    } catch (e) {
+        return [null, e];
+    }
+};
+export default fetchApi;
