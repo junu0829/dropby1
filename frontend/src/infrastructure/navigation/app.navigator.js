101203f92e3349d82cd6fbc6e2f7825977f5a83c
toto9602 <toto9602@naver.com>
2022-01-29 03:55:37 +0900

    [fix]: conflict 해결 시도

--- frontend/src/infrastructure/navigation/app.navigator.js
+++ frontend/src/infrastructure/navigation/app.navigator.js
@@ -6,6 +6,7 @@ import { LogIn } from "../../features/login/LogIn";
 import { Loading } from "../../components/Loading";
 import { NavigationContainer } from "@react-navigation/native";
 import { MapScreen } from "../../features/map/screen/map.screen";
+import { WriteScreen } from "../../features/map/screen/map.screen";
 import DropsOnMap from '../../features/map/screen/map.screen.drops';
 const IntroStack = createStackNavigator();
 
@@ -13,11 +14,12 @@ export const IntroNavigator = () => {
   return (
     <NavigationContainer>
       <IntroStack.Navigator headerMode="none">
-        <IntroStack.Screen name="Drops" component={DropsOnMap} />
         <IntroStack.Screen name="LogIn" component={LogIn} />
         <IntroStack.Screen name="Loading" component={Loading} />
         <IntroStack.Screen name="MapScreen" component={MapScreen} />
+        <IntroStack.Screen name="WriteScreen" component={WriteScreen} />
+        <IntroStack.Screen name="Drops" component={DropsOnMap} />  
       </IntroStack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer> // DropsOnMap은 MapScreen과 사실상 동일. 기능 구분을 용이하게 하기 위해 임시 분리.
   );
 };
