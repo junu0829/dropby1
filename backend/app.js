1cad70e0399adc0b2fe8d51ce62ff94324813e69
toto9602 <toto9602@naver.com>
2022-02-02 13:04:56 +0900

    [fix]: 확인되는 문구만 수정

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,7 @@ ConnectDB();
 
 // request body 안의 데이터를 json 형식으로 변환
 app.use(express.json());
+
 // router는 routes 디렉토리로 분리해 보기
 app.use('/', router);
 
@@ -37,5 +38,5 @@ app.use((req, res) => {
 
 // 서버 포트랑 연결하기
 app.listen(process.env.SERVER_PORT, () => {
-    console.log('Example app listening on port' + process.env.SERVER_PORT);
+    console.log(process.env.SERVER_PORT + '번 포트에 연결되었습니다!');
 })
