88b6cbc12c03bf74584674e5b8534ded3497bf91
toto9602 <toto9602@naver.com>
2022-02-03 03:04:57 +0900

    [feat]: 비밀번호 저장할 때 genSalt 사용으로 변경

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -4,11 +4,16 @@ const jwt = require('jsonwebtoken');
 const {User} = require('../models');
 
 exports.signUp = async ({nickname, email, password}) => {
-    const hashed_pw = await bcrypt.hash(password, nickname);
+    const salt = await bcrypt.genSalt(10);
+    const hashed_pw = await bcrypt.hash(password, salt);
     const user = await User.create({
         nickname,
         email,
         password:hashed_pw
     });
     return user;
+};
+
+exports.logIn = async({email, password}) => {
+
 }
