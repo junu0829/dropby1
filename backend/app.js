16533e6219e78f6a153232851c8bd43f85e9a10c
toto9602 <toto9602@naver.com>
2022-02-02 15:17:00 +0900

    [feat]: user routes 설정

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const ConnectDB = async () => {
         await sequelize.authenticate().then( 
             () => console.log('데이터베이스 연결 성공!')
         );
-        await sequelize.sync({force:true}).then(
+        await sequelize.sync().then(
             () => console.log('동기화 완료!')
         );
     } catch (error) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
+
 const drop = require('./drop/index');
+const user = require('./user/index'); 
 
 router.use('/drops', drop);
-
+router.use('./users', user);
 module.exports = router;
