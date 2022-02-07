8277a78b23fdb93cdd2b78e0e18ed0034c22466f
toto9602 <toto9602@naver.com>
2022-02-07 14:00:55 +0900

    토큰 발급& 검증 middlewares로

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,35 @@
 const jwt = require('jsonwebtoken');
+const { User } = require('../models');
 require('dotenv').config();
 
-exports.verifyAccess = (accessToken) => {
+exports.signAccess = (userData) => { //AccessToken 발급
+    const payload = {
+        pk:userData.pk,
+        email:userData.email
+    }
+
+    return jwt.sign(
+        payload,
+        process.env.JWT_SECRET_ACCESS_KEY,
+        {
+            algorithm:process.env.JWT_ALGORITHM,
+            expiresIn:process.env.JWT_ACCESS_EXPIRE
+        }
+    );
+};
+
+exports.signRefresh = () => { //RefreshToken 발급
+    
+    return jwt.sign(
+        {},
+        process.env.JWT_SECRET_REFRESH_KEY,
+        {
+            algorithm:process.env.JWT_ALGORITHM,
+            expiresIn:process.env.JWT_REFRESH_EXPIRE,
+        }
+    );
+}
+exports.verifyAccess = (accessToken) => { //AccessToken 검증
     try {
         verified = jwt.verify(accessToken, process.env.JWT_SECRET_ACCESS_KEY)
         return {
@@ -17,12 +45,20 @@ exports.verifyAccess = (accessToken) => {
     }
 }
 
-exports.verifyRefresh = (refreshToken) => {
+exports.verifyRefresh = async (refreshToken, userData) => { //RefreshToken 검증
     try {
-        verified = jwt.verify(refreshToken, process.env.JWT_SECRET_REFRESH_KEY)
-        return true;
+        const userToken = await User.findOne({where:{pk:userData.pk}}).Refresh;
+        if (refreshToken === userToken) {
+            try {
+                jwt.verify(refreshToken, process.env.JWT_SECRET_REFRESH_KEY);
+                return true;
+            } catch(error) {
+                return false;
+            }
+        } else {
+            return false;
+        }
     } catch (error) {
         return false;
     }
-
 };
