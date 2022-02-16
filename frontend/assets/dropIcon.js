76a8843a51e9df2f48e87e738e5284e52ff3c84c
wnsn0 <wnsn0829@korea.ac.kr>
2022-02-16 17:29:38 +0900

    [feat] drop detail page

---
+++ frontend/assets/dropIcon.js
@@ -0,0 +1,5 @@
+export default `<svg width="62" height="87" viewBox="0 0 62 87" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="62" height="82" fill="white" fill-opacity="0.01"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M14.4167 73.7031C3.54503 62.9723 3.54503 45.5745 14.4167 34.8437C22.474 26.8911 29.0354 17.4619 34.1013 6.55664C39.1673 17.4619 45.7289 26.8911 53.7862 34.8437C64.6576 45.5745 64.6576 62.9723 53.7862 73.7031C42.9146 84.4338 25.2883 84.4338 14.4167 73.7031Z" fill="#5A3084"/>
+</svg>
+`;
---
+++ frontend/assets/edit.js
@@ -0,0 +1,6 @@
+export default `<svg width="8" height="28" viewBox="0 0 8 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<ellipse cx="4" cy="3.92" rx="4" ry="3.92" fill="#9C4A97"/>
+<ellipse cx="4" cy="14" rx="4" ry="3.92" fill="#9C4A97"/>
+<ellipse cx="4" cy="24.08" rx="4" ry="3.92" fill="#9C4A97"/>
+</svg>
+`;
