856870c58f316bd572f5d218411621dc913305ad
toto9602 <toto9602@naver.com>
2022-02-17 17:04:50 +0900

    [feat]: signUp, signIn 함수 자체를 따로 둬 보기

--- frontend/src/components/utility/auth.js
+++ frontend/src/components/utility/auth.js
@@ -1,4 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import LOCAL_HOST from "../../features/local.js";
 
 export const checkIfTokenExists = () => {
     if (AsyncStorage.getItem('accessToken') && AsyncStorage.getItem('refreshToken')) {
@@ -12,3 +13,51 @@ export const saveToken = (tokens) => {
     AsyncStorage.setItem("refreshToken", tokens.refresh);
 }
 
+export const signIn = async () => {
+    const response = await axios(`http://${LOCAL_HOST}:3000/auth/login`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        data: {
+            email,
+            password,
+        },
+    })
+        .then((res) => {
+            saveToken(res.data.data.tokens);
+            console.log("tokens saved in asyncstorage");
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+
+    return response;
+};
+
+export const signUp = async () => {
+    console.log("signup clicked");
+    const response = await axios(`http://${LOCAL_HOST}:3000/auth/signup`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        data: {
+            nickname,
+            email,
+            password,
+        },
+    })
+        .then((res) => {
+            saveToken(res.data.data.tokens);
+            console.log("tokens saved in asyncstorage");
+        })
+        .catch((error) => {
+            alert("회원가입 오류입니다");
+            console.log(error.message);
+        });
+
+    return response;
+};
