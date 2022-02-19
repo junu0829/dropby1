5b2af7c8485942b8d83d6c0072379178ee9c4ae3
toto9602 <toto9602@naver.com>
2022-02-19 15:21:05 +0900

    [fix]: authServices에서 signRefresh에 pk 정보를 보냄

--- backend/services/authServices.js
+++ backend/services/authServices.js
@@ -41,11 +41,11 @@ exports.logIn = async({email, password}) => {
     const userData = user.dataValues;
 
     const accessToken = signAccess(userData);
-    const refreshToken = signRefresh();
+    const refreshToken = signRefresh(userData.pk);
 
     const tokens = {
-        'access':accessToken,
-        'refresh':refreshToken,
+        access:accessToken,
+        refresh:refreshToken,
     };
     user.Refresh = refreshToken;
     user.save();
@@ -54,49 +54,86 @@ exports.logIn = async({email, password}) => {
     
 
 exports.tokenRefresh = async (accessToken, refreshToken) => {
-    const authResult = verifyAccess(accessToken);
+    const accessResult = verifyAccess(accessToken); //만료되지 않아야만 userData 반환함.
+    console.log(authResult);
 
-    if (authResult.userData === null) { //verify된 데이터가 없음.
+    if (accessResult.userData) { //accessToken이 만료되지 않음. 
         return {
             'success':false,
-            'status':'no verified data',
-            'token':null
-        };
-    } else {
-        const refreshResult = verifyRefresh(refreshToken)
-
-        if (authResult.success === false && authResult.message === 'jwt expired') {
-            if (refreshResult === false) { //accessToken과 refreshToken이 모두 유효하지 않음 -> 재로그인해야 함.
-                return {
-                    'success':false,
-                    'status':'no token valid. re-login required',
-                    'token':null
-                }
-
-            } else { //accessToken은 유효하지 않으나 refreshToken이 유효함. == 새 access 발급.
-
-                const newAccess = signAccess(authResult);
-                return {
-                    'success':true,
-                    'status':'Access Token granted',
-                    'token':{
-                        'access':newAccess,
-                        'refresh':refreshToken
-                    }
-                }
+            'status':'Access Token not expired',
+            'token':{
+                'access':accessToken,
+                'refresh':refreshToken
             }
-        } else { //accessToken이 만료되지 않음
+        }
+    }
+    //accessToken이 만료됨
+    if (accessResult.success === false && accessResult.message === 'jwt expired') { //accessToken은 만료되었고
+        
+        const refreshResult = verifyRefresh(refreshToken); 
+        if (refreshResult === false) { //refreshToken도 유효하지 않음.
             return {
-                'success':false,
-                'status':'Access Token not expired',
-                'token':{
-                    'access':accessToken,
-                    'refresh':refreshToken
+                success:false,
+                status:'No token valid. Re-login required.',
+                token:null
+            }
+        }
+
+        if (refreshResult === true) {
+            const newAccess = signAccess();
+            return {
+                success:true,
+                status:'New Access Token granted',
+                token:{
+                    access:newAccess,
+                    refresh:refreshToken
                 }
             }
         }
     }
 }
+
+    // if (authResult.userData === null) { //verify된 데이터가 없음.
+    //     return {
+    //         'success':false,
+    //         'status':'no verified data',
+    //         'token':null
+    //     };
+    // } else {
+    //     const refreshResult = verifyRefresh(refreshToken)
+
+    //     if (authResult.success === false && authResult.message === 'jwt expired') {
+    //         if (refreshResult === false) { //accessToken과 refreshToken이 모두 유효하지 않음 -> 재로그인해야 함.
+    //             return {
+    //                 'success':false,
+    //                 'status':'no token valid. re-login required',
+    //                 'token':null
+    //             }
+
+    //         } else { //accessToken은 유효하지 않으나 refreshToken이 유효함. == 새 access 발급.
+
+    //             const newAccess = signAccess(authResult);
+    //             return {
+    //                 'success':true,
+    //                 'status':'Access Token granted',
+    //                 'token':{
+    //                     'access':newAccess,
+    //                     'refresh':refreshToken
+    //                 }
+    //             }
+    //         }
+    //     } else { //accessToken이 만료되지 않음
+    //         return {
+    //             'success':false,
+    //             'status':'Access Token not expired',
+    //             'token':{
+    //                 'access':accessToken,
+    //                 'refresh':refreshToken
+    //             }
+    //         }
+    //     }
+    // }
+
 
 exports.logOut = async({authorization, refresh}) => {
     const accessToken = authorization.split('Bearer ')[1];
