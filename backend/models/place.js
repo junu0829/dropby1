ca45a19ffa1afc651182af30f38977557bb43b2d
toto9602 <toto9602@naver.com>
2022-02-22 17:09:19 +0900

    [fix]: Place 모델 수정

--- backend/models/place.js
+++ backend/models/place.js
@@ -1,21 +1,29 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("place",
-    {
-        pk: {
-            type:DataTypes.INTEGER,
-            allowNull:false,
-            primaryKey:true,
-            autoIncrement:true,
-        },
-        name: {
-            type:DataTypes.STRING(20),
-            allowNull:false,
-            unique:true,
-        },
-        coordinates: {
-            type:DataTypes.GEOMETRY('POINT'),
-            allowNull:false,
-        }
-    }
-    )
+const sequelize = require('../config/db');
+const { DataTypes } = require('sequelize');
+
+const Place = sequelize.define("Place", {
+    pk: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+    latitude: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+    longitude: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    },
+}, {
+    freezeTableName: true,
+    timestamps: false
 }
+);
+
+module.exports = Place
