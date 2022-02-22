fc8b0cf1862f06e6d742fe8dbbab49383888b974
toto9602 <toto9602@naver.com>
2022-02-22 17:30:51 +0900

    [feat]: place router 추가

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,10 @@ const router = require("express").Router();
 
 const drop = require("./dropRouter");
 const auth = require("./authRouter");
+const place = require('./placeRouter');
 
 router.use("/drops", drop);
 router.use("/auth", auth);
+router.use("/places", place);
 module.exports = router;
 
