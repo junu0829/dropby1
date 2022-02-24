21921ea98c777209f6a0657027dc24be3e8a0d88
toto9602 <toto9602@naver.com>
2022-02-24 20:12:56 +0900

    router수정_mergeparams

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 
-
 const drop = require("./dropRouter");
 const auth = require("./authRouter");
 const place = require('./placeRouter');
 
+const passPlacePk = (req, res, next) => {
+    req.placePk = req.params.placePk;
+    next();
+}
 router.use("/", place);
 router.use("/auth", auth);
-router.use("/:placeId/drops", drop);
+router.use("/:placePk/drops", drop);
 
 module.exports = router;
-
